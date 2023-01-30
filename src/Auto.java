02678390cce4996a9f0a59f75f0c1a6528b75536
Arikpele <araratpeleshyan@gmail.com>
2023-01-30 21:29:01 +0300

    Задание

--- src/Auto.java
+++ src/Auto.java
@@ -1,6 +1,6 @@
 public class Auto {
-    private String brand;
-    private String model;
+    private String brand = "default";
+    private String model = "default";
     private double engineVolume;
     private String color;
     private int year;
@@ -13,9 +13,28 @@ public class Auto {
         this.color = color;
         this.engineVolume = engineVolume;
         this.model = model;
+        if (brand == null || brand.equals("") || brand.equals(" ")) {
+            this.brand = "default";
+        }
+        if (model.equals("Null") || model.equals("") || model.equals(" ")) {
+            this.model = "default";
+        }
+        if (country.equals("Null") || country.equals("") || country.equals(" ")) {
+            this.country = "default";
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        }
+        if (color.equals("Null") || color.equals("") || color.equals(" ")) {
+            this.color = "белого";
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        }
     }
 
     public String toString() {
         return "ID: | Модель:" + brand + " " + model + "|" + year + " год выпуска |" + "сборка в " + country + "|" + color + " цвет кузова" + "| объем двигателя - " + engineVolume;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,13 @@ public class Main {
                     new Auto("Audi", "A8 50l TDI quattro", 2020, "Германия", "черный", 3.0),
                     new Auto("BMW", "Z8", 2021, "Германия", "черного", 3.0),
                     new Auto("KIA", "Sportage 4-го поколения", 2018, "Южная корея", "красный", 2.4),
-                    new Auto("Hyundai ", "Avante", 2016, "Южная корея", "Оранжевый", 1.6)
+                    new Auto("Hyundai ", "Avante", 2016, "Южная корея", "Оранжевый", 1.6),
+                    new Auto(null, " ", 0, "", "", 0)
             };
 
     public static void printAuto() {
         Arrays.stream(auto).forEach(System.out::println);
+
     }
 
 
