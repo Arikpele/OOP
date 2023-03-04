676edc4eab5bc217ab3f4a826b0e103d23541b6e
Arikpele <79112835522@y.ru>
2023-03-04 15:48:49 +0300

    задание 2

--- src/rally/Servicestation.java
+++ src/rally/Servicestation.java
@@ -1,24 +1,34 @@
 package rally;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Servicestation {
+public class Servicestation<T extends Car> {
+    private Queue<T> queue = new LinkedList<>();
+
+    public Servicestation(Queue<T> queue, String name) {
+        this.queue = queue;
+        this.name = name;
+    }
+
     public String name;
 
-    public String getName() {
-        return name;
+    public void addCar1(T Car) {
+        queue.offer(Car);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void osmotr() {
+       T car= queue.poll();
+       if (car != null) {
+           System.out.println("провести техосмотр авто");
+           osmotr();
+       }
+       else {
+           System.out.println("Очередь закончилась");
+       }
     }
 
-    public Servicestation(String name) {
-        this.name = name;
+    public int size() {
+        return queue.size();
     }
 
-    @Override
-    public String toString() {
-        return "Servicestation{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
